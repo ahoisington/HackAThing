c115febf3a098201c2efbefd6b8e3d4047005bfa
Morgan Philie <morgan@MacBook-Pro.local>
2018-01-07 13:54:11 -0500

    calculated winner

--- src/index.js
+++ src/index.js
@@ -21,6 +21,9 @@ class Board extends React.Component {
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (calculateWinner(squares) || squares[i]) {
+      return;
+    }
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       squares: squares,
@@ -37,9 +40,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
     const greeting = 'Welcome to Acacia and Morgan\'s Hack-A-Thing!!!! We made a tic-tac-toe board with react.  Have fun!\n\n';
-
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner) {
+      status = 'Player ' + winner + ' has won!';
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
     return (
       <div>
       <div className="greeting">{greeting}</div>
@@ -93,6 +101,27 @@ class ShoppingList extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares [a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
